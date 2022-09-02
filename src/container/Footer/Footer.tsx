360969591fe97d44f7d1cf6a8f9534cd1efe0139
Fastovec <sergejfastovec284@gmail.com>
2022-09-02 17:49:03 +0300

    added columns in Footer

--- src/container/Footer/Footer.tsx
+++ src/container/Footer/Footer.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './Footer.scss'
 import { LogoFooter } from './LogoFooter'
-import { Grid, Box } from '@mui/material'
-import ImageList from '@mui/material/ImageList'
-import ImageListItem from '@mui/material/ImageListItem'
+import { Grid, CardMedia, Card } from '@mui/material'
+import { Social } from '../Header/Social'
 
 type Props = {}
 
@@ -12,39 +11,99 @@ const Footer = (props: Props) => {
         <div className="footer-site">
             <div className="footer-top">
                 <div className="footer-container">
-                    <Box>
-                        <Grid container spacing={3}>
-                            <Grid item xs={2} md={2}>
-                                <LogoFooter />
-                            </Grid>
-                            <Grid item xs={4} md={4}>
-                                <div className="widget-post-bx">
-                                    <ImageList
-                                        sx={{ width: 90, height: 90 }}
-                                        cols={1}
-                                    >
-                                        <ImageListItem>
-                                            <img
-                                                src="/img/ledy_in_white.jpg"
-                                                alt="wooman in white"
-                                            />
-                                        </ImageListItem>
-                                    </ImageList>
-
+                    <Grid container spacing={2}>
+                        <Grid item md={2}>
+                            <LogoFooter />
+                        </Grid>
+                        <Grid item md={4}>
+                            <Grid container spacing={2}>
+                                <Grid item md={12}>
+                                    <h6 className="widget-title ">
+                                        <span>RECENT POST</span>
+                                    </h6>
+                                </Grid>
+                                <Grid item md={6}>
+                                    <Card sx={{ maxWidth: '80px' }}>
+                                        <CardMedia
+                                            component="img"
+                                            height="80px"
+                                            image="/img/ledy_in_white.jpg"
+                                            alt="wooman in white"
+                                        />
+                                    </Card>
+                                </Grid>
+                                <Grid item md={6}>
                                     <div className="widget-post-info">
                                         <div>Story Of Beaty</div>
                                         <div>OCT 1, 2022</div>
                                     </div>
-                                </div>
-                            </Grid>
-                            <Grid item xs={4} md={4}>
-                                xs=6 md=4
-                            </Grid>
-                            <Grid item xs={2} md={2}>
-                                xs=6 md=8
+                                </Grid>
+
+                                <Grid item md={6}>
+                                    <Card sx={{ maxWidth: '80px' }}>
+                                        <CardMedia
+                                            component="img"
+                                            height="80px"
+                                            image="/img/wooman_near_water.jpg"
+                                            alt="wooman in white"
+                                        />
+                                    </Card>
+                                </Grid>
+                                <Grid item md={6}>
+                                    <div className="widget-post-info">
+                                        <div>Story Of Beaty</div>
+                                        <div>OCT 1, 2022</div>
+                                    </div>
+                                </Grid>
+
+                                <Grid item md={6}>
+                                    <Card sx={{ maxWidth: '80px' }}>
+                                        <CardMedia
+                                            component="img"
+                                            height="80px"
+                                            image="/img/wooman_in_bike.jpg"
+                                            alt="wooman_in_bike"
+                                        />
+                                    </Card>
+                                </Grid>
+                                <Grid item md={6}>
+                                    <div className="widget-post-info">
+                                        <div>Story Of Beaty</div>
+                                        <div>OCT 1, 2022</div>
+                                    </div>
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </Box>
+                        <Grid item md={4}>
+                            <Grid container spacing={2}>
+                                <Grid item md={12}>
+                                    <h6 className="widget-title">
+                                        <span>MY BLOGS</span>
+                                    </h6>
+                                </Grid>
+                                <Grid item md={12}>
+                                    <Card sx={{ maxWidth: '100%' }}>
+                                        <CardMedia
+                                            component="img"
+                                            height="90%"
+                                            image="https://lemars.wprdx.com/demo/wp-content/themes/lemars/assets/images/footer-video.jpg"
+                                            alt="wooman in white"
+                                        />
+                                    </Card>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item md={2}>
+                            <Grid item md={12}>
+                                <h6 className="widget-title">
+                                    <span>SOCIAL ICON</span>
+                                </h6>
+                            </Grid>
+                            <Grid item md={12}>
+                                <Social />
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </div>
             </div>
             <div className="footer-bottom"></div>
