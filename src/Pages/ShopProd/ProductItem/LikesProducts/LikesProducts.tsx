1d8104a3ce3414a3bbb52e7269a761f6ad5db082
Fastovec <sergejfastovec284@gmail.com>
2022-10-01 17:06:16 +0300

    added buttonsLikeState  to LikesProducts

--- src/Pages/ShopProd/ProductItem/LikesProducts/LikesProducts.tsx
+++ src/Pages/ShopProd/ProductItem/LikesProducts/LikesProducts.tsx
@@ -1,17 +1,69 @@
 import GridContainerBottom from 'container/Main/GridContainerBottom/GridContainerBottom'
-import React from 'react'
+import React, { Fragment } from 'react'
+import { useAppSelector } from 'redux/hooks'
 import ShopBg from '../ShopBg'
+import {
+    arrProducts,
+    ArrProductsProps,
+    getProductObject,
+} from '../../../../components/Arrays/arraysProducts'
+import ShopListContent from '../ShopListContent'
 
-type Props = {}
+type Props = {
+    productObject?: { [id: number]: ArrProductsProps }
+}
 
-const LikesProducts = (props: Props) => {
+const LikesProducts = ({
+    productObject = getProductObject(arrProducts),
+}: Props) => {
+    const likesProductsShow = useAppSelector((state) => state.buttonsState)
+    console.log(likesProductsShow)
     return (
         <>
             <ShopBg />
 
-            <div className="container-pages">
-                <div className="container"></div>
+            {/* <div className="container-pages"> */}
+            <div className="container">
+                {likesProductsShow ? (
+                    <div className="">
+                        {Object.keys(likesProductsShow).map((productId) => (
+                            <Fragment key={parseInt(productId)}>
+                                {likesProductsShow[parseInt(productId)] ===
+                                true ? (
+                                    <ShopListContent
+                                        id={parseInt(productId)}
+                                        img={
+                                            productObject[parseInt(productId)]
+                                                .img
+                                        }
+                                        alt={
+                                            productObject[parseInt(productId)]
+                                                .alt
+                                        }
+                                        price={
+                                            productObject[parseInt(productId)]
+                                                .price
+                                        }
+                                        discount={
+                                            productObject[parseInt(productId)]
+                                                .discount
+                                        }
+                                        nameProduct={
+                                            productObject[parseInt(productId)]
+                                                .nameProduct
+                                        }
+                                    />
+                                ) : undefined}
+                            </Fragment>
+                        ))}
+                    </div>
+                ) : (
+                    <div style={{ fontSize: '48px', textAlign: 'center' }}>
+                        There are no likes-products on this page
+                    </div>
+                )}
             </div>
+            {/* </div> */}
             <div className="container bottom">
                 <GridContainerBottom />
             </div>
