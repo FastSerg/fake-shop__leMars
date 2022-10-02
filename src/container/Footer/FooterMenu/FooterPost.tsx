05560a2abe2dc2e64e209dbb005c7e1f62a28490
Fastovec <sergejfastovec284@gmail.com>
2022-10-02 21:14:13 +0300

    changed the arrangement of blocks when expanding sm  the footer screen

--- src/container/Footer/FooterMenu/FooterPost.tsx
+++ src/container/Footer/FooterMenu/FooterPost.tsx
@@ -1,32 +1,6 @@
 import React from 'react'
 import { Grid, CardMedia, Card } from '@mui/material'
-
-// const itemPages = [
-//     { id: 1, img: '/img/ledy_in_white_full.jpg', alt: 'ledy_in_white' },
-//     { id: 2, img: '/img/wooman_near_water.jpg', alt: 'wooman_near_water' },
-//     { id: 3, img: '/img/wooman_in_bike.jpg', alt: 'wooman_near_water' },
-//     { id: 4, img: '/img/motoki-tonn.jpg', alt: 'motoki-tonn' },
-// ]
-
-// type itemDataProps = { id: number; img: string; alt: string }
-
-// {itemPages.map(({ id, alt, img }: itemDataProps) => (
-//     <Grid item md={6}>
-//         {' '}
-//         <Card key={id}>
-//             <CardMedia
-//                 component="img"
-//                 image={img}
-//                 alt={alt}
-//                 sx={{
-//                     width: '80px',
-//                     height: '80px',
-//                     backgroundSize: 'cover',
-//                 }}
-//             />
-//         </Card>
-//     </Grid>
-// ))}
+import { Link } from 'react-router-dom'
 
 type Props = {}
 
@@ -34,12 +8,13 @@ const FooterPost = (props: Props) => {
     return (
         <Grid item md={3} sm={12}>
             <Grid container spacing={2}>
-                <Grid item md={12}>
+                <Grid item md={12} sm={12}>
                     <h6 className="widget-title ">RECENT POST</h6>
                 </Grid>
                 <Grid
                     item
                     md={6}
+                    sm={2}
                     sx={{ display: 'flex', justifyContent: 'flex-end' }}
                 >
                     <Card>
@@ -55,14 +30,18 @@ const FooterPost = (props: Props) => {
                         />
                     </Card>
                 </Grid>
-                <Grid item md={6}>
-                    <div className="widget-post-info">
-                        <div>Story Of Beaty</div>
-                        <div>OCT 1, 2022</div>
-                    </div>
+
+                <Grid item md={6} sm={2}>
+                    <Link to={'/storyOfBeauty'}>
+                        <div className="widget-post-info">
+                            <div>Story Of Beauty</div>
+                            <div>OCT 1, 2022</div>
+                        </div>
+                    </Link>
                 </Grid>
                 <Grid
                     item
+                    sm={2}
                     md={6}
                     sx={{ display: 'flex', justifyContent: 'flex-end' }}
                 >
@@ -79,16 +58,19 @@ const FooterPost = (props: Props) => {
                         />
                     </Card>
                 </Grid>
-                <Grid item md={6}>
-                    <div className="widget-post-info">
-                        <div>Everything About Cultural</div>
-                        <div>OCT 1, 2022</div>
-                    </div>
-                </Grid>
 
+                <Grid item md={6} sm={2}>
+                    <Link to={'/everything'}>
+                        <div className="widget-post-info">
+                            <div>Everything About Cultural</div>
+                            <div>OCT 1, 2022</div>
+                        </div>
+                    </Link>
+                </Grid>
                 <Grid
                     item
                     md={6}
+                    sm={2}
                     sx={{ display: 'flex', justifyContent: 'flex-end' }}
                 >
                     <Card>
@@ -104,11 +86,13 @@ const FooterPost = (props: Props) => {
                         />
                     </Card>
                 </Grid>
-                <Grid item md={6}>
-                    <div className="widget-post-info">
-                        <div>Las Catalines Welcome</div>
-                        <div>OCT 1, 2022</div>
-                    </div>
+                <Grid item md={6} sm={2}>
+                    <Link to={'/catalines'}>
+                        <div className="widget-post-info">
+                            <div>Las Catalines Welcome</div>
+                            <div>OCT 1, 2022</div>
+                        </div>
+                    </Link>
                 </Grid>
             </Grid>
         </Grid>
