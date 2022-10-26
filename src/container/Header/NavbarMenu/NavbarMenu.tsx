340ee00be789abcf851862b4802a098de05b5914
Fastovec <sergejfastovec284@gmail.com>
2022-10-26 14:01:33 +0300

    deleted page Home

--- src/container/Header/NavbarMenu/NavbarMenu.tsx
+++ src/container/Header/NavbarMenu/NavbarMenu.tsx
@@ -1,5 +1,4 @@
 import Category from './ElementsNavbarMenu/Category'
-import Home from './ElementsNavbarMenu/Home'
 import './NavbarMenu.scss'
 import Pages from './ElementsNavbarMenu/Pages'
 import PostLayout from './ElementsNavbarMenu/PostLayout'
@@ -10,14 +9,13 @@ export const NavbarMenu = () => {
     return (
         <nav className="navbar-menu">
             <ul className="navbar-menu__list">
-                <Home />
                 <PostLayout />
                 <Category />
                 <Shop />
                 <Pages />
                 <NavLink to="/contactUs">
                     <li className="contact">
-                        <div className="menu-item">CONTACT </div>
+                        <div className="menu-item">CONTACT US</div>
                     </li>
                 </NavLink>
             </ul>
